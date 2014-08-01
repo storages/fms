aaa63e821fc8b906ea10b12640272dea1f30bd7a
hwd <314454759@qq.com>
2014-08-01 15:08:36 +0800

    我再写一次。

---
+++ fms/src/com/fms/commons/CommonConstant.java
@@ -0,0 +1,13 @@
+package com.fms.commons;
+
+public class CommonConstant {
+
+		
+		public static final String LOGINUSER="loginUser";  //µÇÂ¼ÓÃ»§
+		public static final String LOGIN_USER_NAME="loginUserName"; //µÇÂ¼ÕËºÅ
+		public static final String LOGIN_USER_PASSWORD="loginUserPassword";  //ÃÜÂë
+
+	
+
+
+}

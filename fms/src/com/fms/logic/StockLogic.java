3642f71e3415f00251709e762541cee125eb643f
gdc <1206233977@qq.com>
2014-08-05 22:54:21 +0800

    完成仓库管理

---
+++ fms/src/com/fms/logic/StockLogic.java
@@ -0,0 +1,56 @@
+package com.fms.logic;
+
+import java.util.List;
+
+import com.fms.core.entity.Stock;
+
+public interface StockLogic {
+
+	/**
+	 * 查询所有的仓库信息[分页]
+	 * 
+	 * @param likeStr
+	 * @param index
+	 * @param length
+	 * @return
+	 */
+	public List<Stock> findAllStock(String likeStr, Integer index,
+			Integer length);
+
+	/**
+	 * 查询数据的条数
+	 * 
+	 * @param className
+	 * @param name
+	 * @return
+	 */
+	public Integer findDataCount(String className, String name);
+
+	/**
+	 * 根据id查询仓库
+	 * 
+	 * @param id
+	 * @return
+	 */
+	public Stock findStockById(String id);
+
+	/**
+	 * 保存单个实体
+	 * 
+	 * @param stock
+	 */
+	public void saveStock(Stock stock);
+
+	/**
+	 * 查找仓库编码是否重复
+	 * 
+	 * @param code
+	 * @return
+	 */
+	public String findStockByCode(String code);
+
+	/**
+	 * 根据Id来删除仓库信息
+	 */
+	public void delStockById(String[] ids);
+}

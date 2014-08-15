102bb8c3ee1c37afc7d18c617d56db2b2585bfbc
gdc <1206233977@qq.com>
2014-08-15 20:46:50 +0800

    优化删除，如果有被引用会提示

--- fms/src/com/fms/action/SettlementAction.java
+++ fms/src/com/fms/action/SettlementAction.java
@@ -93,14 +93,33 @@ public class SettlementAction extends BaseAction {
 	 * 删除结算方式
 	 * @return
 	 */
-	public String delSettlById(){
+	public void delSettlById(){
 		if (null != ids && !"".equals(ids)) {
 			String [] idArr = ids.split(",");
 			if(idArr!=null && idArr.length>0){
-				this.settlementLogic.delSettltById(idArr);
+				PrintWriter out = null;
+				AjaxResult  result=new AjaxResult();
+				try {
+					out = response.getWriter();
+					response.setContentType("application/text");
+					response.setCharacterEncoding("UTF-8");
+					this.settlementLogic.delSettltById(idArr);
+					result.setSuccess(true);
+					result.setMsg("删除成功！");
+					JSONObject json=new JSONObject(result);
+					out.println(json.toString());
+					out.flush();
+					out.close();
+				} catch (Exception e) {
+					result.setSuccess(false);
+					result.setMsg("数据被其它地方引用，不能删除！");
+					JSONObject json=new JSONObject(result);
+					out.println(json.toString());
+					out.flush();
+					out.close();
+				}
 			}
 		}
-		return "save";
 	}
 	
 	/**
